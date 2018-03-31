71a3357caac85f711be60c956d18c490591b6cd8
Admin <admin@admins-mbp-2.lan>
2018-03-30 20:31:35 -0400

    code layout organized

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -13,35 +13,35 @@ document.onkeyup = function(event) {
 
   var currentGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    if (guessesLogged.indexOf(currentGuess) < 0 && alphabet.indexOf(currentGuess) >= 0){
-      guessesLogged[guessesLogged.length] = currentGuess;//use push to guessesLogged?
-      guessesLeft--;
-    };
+  if (guessesLogged.indexOf(currentGuess) < 0 && alphabet.indexOf(currentGuess) >= 0) {
+    guessesLogged[guessesLogged.length] = currentGuess;
+    guessesLeft--;
+  };
 
-    if (psychicLetter === currentGuess) {
-      wins++;
-      alert("You Win!");
-      guessesLeft = 12;
-      guessesLogged = [];
-      psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-      console.log("psychic letter: " + psychicLetter);
-      };
+  if (psychicLetter === currentGuess) {
+    wins++;
+    alert("You Win!");
+    guessesLeft = 12;
+    guessesLogged = [];
+    psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("psychic letter: " + psychicLetter);
+  };
 
-    if (guessesLeft === 0) {
-      losses++;
-      alert("You Lost!");
-      guessesLeft = 12;
-      guessesLogged = [];
-      psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-      console.log("psychic letter: " + psychicLetter);
-    };
+  if (guessesLeft === 0) {
+    losses++;
+    alert("You Lost!");
+    guessesLeft = 12;
+    guessesLogged = [];
+    psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("psychic letter: " + psychicLetter);
+  };
 
-//HTML
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
-    document.getElementById("guessesLogged").innerHTML = guessesLogged;
-    var resetButton = document.querySelector("#resetButton");
-    resetButton.style.cursor = "pointer";
+  //HTML
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("losses").innerHTML = losses;
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  document.getElementById("guessesLogged").innerHTML = guessesLogged;
+  var resetButton = document.querySelector("#resetButton");
+  resetButton.style.cursor = "pointer";
 
 }
